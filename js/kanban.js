b383849b563cc0fe43984b87cc8de2c10503ee3e
Daniel Abramson <daniel.abramson@ruralsource.com>
2019-11-05 14:43:18 -0700

    Completed module 1

--- js/kanban.js
+++ js/kanban.js
@@ -1,12 +1,12 @@
 let order = 1;
 let adding = false;
 
-const error = document.querySelector('.error');
-const message = 'Please add a description.';
+const error = document.querySelector(".error");
+const message = "Please add a description.";
 
-const add_btn = document.querySelector('.add');
-add_btn.addEventListener('click', () => {
-  const target = document.querySelector('#requested');
+const add_btn = document.querySelector(".add");
+add_btn.addEventListener("click", () => {
+  const target = document.querySelector("#requested");
   if (adding == false) {
     adding = true;
     target.appendChild(create_item());
@@ -16,7 +16,47 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  var item = document.createElement("div");
+  item.classList.add("item");
+  item.id = "item-" + order;
+  item.draggable = true;
+
+  item.addEventListener("dragstart", event => {
+    return event.dataTransfer.setData("text", event.target.id);
+  });
+  item.addEventListener("dragend", event => {
+    return event.dataTransfer.clearData();
+  });
+
+  var input = document.createElement("input");
+  item.appendChild(input);
+
+  var save_btn = document.createElement("button");
+  save_btn.innerHTML = "Save";
+
+  save_btn.addEventListener("click", () => {
+    error.innerHTML = "";
+    if (input.value != "") {
+      order++;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
+  });
+
+  item.appendChild(save_btn);
+
+  return item;
 };
 
-document.querySelectorAll('.drop').forEach(element => {
+document.querySelectorAll(".drop").forEach(element => {
+  element.addEventListener("drop", event => {
+    event.preventDefault();
+    var id = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(id));
+  });
+  element.addEventListener("dragover", event => {
+    event.preventDefault();
+  });
 });
